eeae4a0fea82602025a6811af39df9746c974d82
ayushagarwal <ayushagarwal@salesforce.com>
2025-06-20 18:06:41 +0530

    Refactor - use ParametersDTO to reduce constructor params

--- src/main/java/com/mulesoft/connectors/inference/internal/connection/types/VisionModelConnection.java
+++ src/main/java/com/mulesoft/connectors/inference/internal/connection/types/VisionModelConnection.java
@@ -2,6 +2,7 @@ package com.mulesoft.connectors.inference.internal.connection.types;
 
 import org.mule.runtime.http.api.client.HttpClient;
 
+import com.mulesoft.connectors.inference.internal.dto.ParametersDTO;
 import com.mulesoft.connectors.inference.internal.service.VisionModelService;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,6 +23,15 @@ public class VisionModelConnection extends BaseConnection {
     this.maxTokens = maxTokens;
     this.temperature = temperature;
     this.topP = topP;
+    this.setBaseService(getVisionModelService());
+  }
+
+  protected VisionModelConnection(HttpClient httpClient, ObjectMapper objectMapper, ParametersDTO parametersDTO, String apiUrl) {
+    super(httpClient, objectMapper, parametersDTO.modelName(), parametersDTO.apiKey(),
+          parametersDTO.timeout(), apiUrl);
+    this.maxTokens = parametersDTO.maxTokens();
+    this.temperature = parametersDTO.temperature();
+    this.topP = parametersDTO.topP();
     this.setBaseService(getVisionModelService());
   }
 
