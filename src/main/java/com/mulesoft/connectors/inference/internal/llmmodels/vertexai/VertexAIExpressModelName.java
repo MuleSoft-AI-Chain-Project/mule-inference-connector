09ba5dea6ab6586bd6221a0c982dce8637bdc477
ayushagarwal <ayushagarwal@salesforce.com>
2025-07-14 19:13:05 +0530

    Update models for vertex AI express

--- src/main/java/com/mulesoft/connectors/inference/internal/llmmodels/vertexai/VertexAIExpressModelName.java
+++ src/main/java/com/mulesoft/connectors/inference/internal/llmmodels/vertexai/VertexAIExpressModelName.java
@@ -2,11 +2,11 @@ package com.mulesoft.connectors.inference.internal.llmmodels.vertexai;
 
 public enum VertexAIExpressModelName {
 
-  GEMINI_20_FLASH_001("gemini-2.0-flash-001", true, true), GEMINI_20_FLASH_LITE_001("gemini-2.0-flash-lite-001", true,
-      true), GEMINI_20_PRO_EXP_02_05("gemini-2.0-pro-exp-02-05", true, true), // Experimental
-  GEMINI_15_FLASH_002("gemini-1.5-flash-002", true, true), // expires 9/24/25
-  GEMINI_15_PRO_002("gemini-1.5-pro-002", true, true), // expires 9/24/25
-  GEMINI_10_PRO_002("gemini-1.0-pro-002", true, true); // expires 4/9/25
+  GEMINI_25_FLASH("gemini-2.5-flash", true, true), GEMINI_25_PRO("gemini-2.5-pro", true,
+      true), GEMINI_25_FLASH_LITE_PREVIEW_EXP_06_17("gemini-2.5-flash-lite-preview-06-17", true,
+          true), GEMINI_20_FLASH_001("gemini-2.0-flash-001", true, true), GEMINI_20_FLASH_LITE_001("gemini-2.0-flash-lite-001",
+              true, true), GEMINI_15_FLASH_002("gemini-1.5-flash-002", true, true), // expires 9/24/25
+  GEMINI_15_PRO_002("gemini-1.5-pro-002", true, true); // expires 9/24/25
 
   private final String value;
   private final boolean textGenerationSupport;
