1639bc9d28553f7b29e14272956b137602f9bd90
ayush-salesforce <162944854+ayush-salesforce@users.noreply.github.com>
2025-07-24 13:47:24 +0530

    Feature/fix read image url and other munits (#195)
    * Fix read image munit test URL
    
    * Skip munits for xinference as current deployed version does not support tools
    
    * Update vision test url

--- src/main/java/com/mulesoft/connectors/inference/internal/connection/provider/VisionModelConnectionProvider.java
+++ src/main/java/com/mulesoft/connectors/inference/internal/connection/provider/VisionModelConnectionProvider.java
@@ -27,10 +27,10 @@ public abstract class VisionModelConnectionProvider extends BaseConnectionProvid
     logger.debug("Validating VisionModelConnection... ");
     try {
       var resp = connection.getVisionModelService().readImage(connection, "What do you see?",
-                                                              "https://tripfixers.com/wp-content/uploads/2019/11/eiffel-tower-with-snow.jpeg");
+                                                              "https://cdn.britannica.com/61/93061-050-99147DCE/Statue-of-Liberty-Island-New-York-Bay.jpg");
       var textResponse = this.getObjectMapper().readValue(resp.getOutput(), TextGenerationResponse.class);
 
-      if (textResponse.response().contains("Eiffel Tower")) {
+      if (textResponse.response().contains("Statue of Liberty")) {
         return ConnectionValidationResult.success();
       }
     } catch (IOException | TimeoutException | ModuleException e) {
