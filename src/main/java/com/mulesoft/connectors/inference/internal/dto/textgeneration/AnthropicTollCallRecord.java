89cd440875d1e99031e09a7fb9df60030c755dc1
ayushagarwal <ayushagarwal@salesforce.com>
2025-08-19 15:30:58 +0530

    Robust tooling support by adding more fields to function schema

--- src/main/java/com/mulesoft/connectors/inference/internal/dto/textgeneration/AnthropicTollCallRecord.java
+++ src/main/java/com/mulesoft/connectors/inference/internal/dto/textgeneration/AnthropicTollCallRecord.java
@@ -1,8 +1,8 @@
 package com.mulesoft.connectors.inference.internal.dto.textgeneration;
 
-import com.mulesoft.connectors.inference.api.request.Parameters;
+import com.mulesoft.connectors.inference.api.request.FunctionSchema;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)public record AnthropicTollCallRecord(String name,String description,Parameters inputSchema){}
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)public record AnthropicTollCallRecord(String name,String description,FunctionSchema inputSchema){}
