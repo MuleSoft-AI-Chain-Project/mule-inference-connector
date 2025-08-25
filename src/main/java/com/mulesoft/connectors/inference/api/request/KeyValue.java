0818ec25f509bb24b4fed63dd88e23b002944ee0
ayushagarwal <ayushagarwal@salesforce.com>
2025-08-25 15:11:19 +0530

    Add munit and change value to support expression

--- src/main/java/com/mulesoft/connectors/inference/api/request/KeyValue.java
+++ src/main/java/com/mulesoft/connectors/inference/api/request/KeyValue.java
@@ -2,6 +2,7 @@ package com.mulesoft.connectors.inference.api.request;
 
 import static org.mule.runtime.api.meta.ExpressionSupport.NOT_SUPPORTED;
 
+import org.mule.runtime.api.meta.ExpressionSupport;
 import org.mule.runtime.extension.api.annotation.Expression;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 
@@ -15,7 +16,7 @@ public abstract class KeyValue {
   private String key;
 
   @Parameter
-  @Expression(NOT_SUPPORTED)
+  @Expression(ExpressionSupport.SUPPORTED)
   private String value;
 
   public void setKey(String key) {
